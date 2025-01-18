2cde662866a3e083a87a07a26b68c2136d513f3b
Kosta-111 <nestt@ukr.net>
2025-01-19 01:29:57 +0200

    Spring Boot MyAPI

---
+++ 4.SpringBoot/MyAPI/src/main/java/org/example/service/CategoryService.java
@@ -0,0 +1,19 @@
+package org.example.service;
+
+import org.example.entites.CategoryEntity;
+import org.example.repository.ICategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    //Автоматично робиться Dependency Injection
+    @Autowired
+    private ICategoryRepository categoryRepository;
+
+    public List<CategoryEntity> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+}

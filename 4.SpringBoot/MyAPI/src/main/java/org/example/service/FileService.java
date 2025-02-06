0e69da702d31ed377cd70eabcc71abe1288f0fcf
Kosta-111 <nestt@ukr.net>
2025-02-06 03:14:28 +0200

    improve file service

--- 4.SpringBoot/MyAPI/src/main/java/org/example/service/FileService.java
+++ 4.SpringBoot/MyAPI/src/main/java/org/example/service/FileService.java
@@ -1,13 +1,17 @@
 package org.example.service;
 
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -16,24 +20,21 @@ public class FileService {
     @Value("${upload.dir}")
     private String uploadDir;
 
+    private String extension = "webp";
+
+    private Map<String, Integer> sizeMap = Map.of(
+            "small", 30,
+            "medium", 100,
+            "large", 1000
+    );
+
     public String load(MultipartFile file) {
         // Перевіряємо, чи файл не порожній
         if (file.isEmpty()) return "";
 
         try (var inputStream = file.getInputStream()) {
-            Files.createDirectories(Paths.get(uploadDir));  //створить якщо не існує
-
-            // Отримуємо ім'я файлу та створюємо шлях для збереження
-            var fileName = file.getOriginalFilename();
-            var fileExt = fileName != null && fileName.contains(".")
-                    ? fileName.substring(fileName.lastIndexOf("."))
-                    : "";
-            var newFileName = UUID.randomUUID() + fileExt;
-            Path filePath = Paths.get(uploadDir, newFileName);
-
-            // Зберігаємо файл на сервер
-            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
-            return newFileName;
+            var fileName = saveStreamToFile(inputStream);
+            return fileName;
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -47,18 +48,8 @@ public class FileService {
             return "";
 
         try (var inputStream = new URL(imageUrl).openStream()) {
-            Files.createDirectories(Paths.get(uploadDir));  //створить якщо не існує
-
-            // Отримуємо ім'я файлу та створюємо шлях для збереження
-            var fileExt = imageUrl.contains(".")
-                    ? imageUrl.substring(imageUrl.lastIndexOf("."))
-                    : "";
-            var newFileName = UUID.randomUUID() + fileExt;
-            Path filePath = Paths.get(uploadDir, newFileName);
-
-            // Зберігаємо файл на сервер
-            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
-            return newFileName;
+            var fileName = saveStreamToFile(inputStream);
+            return fileName;
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -66,10 +57,27 @@ public class FileService {
         }
     }
 
+    private String saveStreamToFile(InputStream stream) throws IOException {
+        Files.createDirectories(Paths.get(uploadDir));  //створить якщо не існує
+        String fileName = UUID.randomUUID() + "." + extension;
+        var bufferedImage = ImageIO.read(stream);
+
+        for (var entry : sizeMap.entrySet()) {
+            var folder = entry.getKey();
+            var size = entry.getValue();
+            Files.createDirectories(Paths.get(uploadDir, folder));
+            String filePath = Paths.get(uploadDir, folder, fileName).toString();
+            Thumbnails.of(bufferedImage).size(size,size).outputFormat(extension).toFile(filePath);
+        }
+        return fileName;
+    }
+
     public void remove(String fileName) {
         try {
-            Path filePath = Paths.get(uploadDir, fileName);
-            Files.deleteIfExists(filePath);
+            for (String folder : sizeMap.keySet()) {
+                Path filePath = Paths.get(uploadDir, folder, fileName);
+                Files.deleteIfExists(filePath);
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
